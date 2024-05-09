7272626c82772ec86777a2ebf76e68fb50146ebb
thanhdatdev <thanddat.nguyen2104@gmail.com>
2024-05-09 23:00:26 +0700

    Refactor: Dockerfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,9 +6,10 @@ task :deploy => :remote_environment do
   set :user, 'deploy'
   set :domain, '18.141.12.245'
   command %[cd /home/deploy/youtube-video-sharing-app]
-  # command %[git pull origin main]
+  command %[git pull origin main]
   command %[docker compose build]
   command %[docker compose down]
   command %[docker compose up -d]
   command %[docker system prune -f]
+  print("Deploy successful!")
 end
